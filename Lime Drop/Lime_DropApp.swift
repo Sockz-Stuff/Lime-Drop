d92f1bdf0521c8807a70d9bbbb5e310615705f5f
Sockzzz <86269797+Sockzzz@users.noreply.github.com>
2023-01-22 12:06:43 -0800

    Initial Commit

---
+++ Lime Drop/Lime_DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lime_DropApp.swift
+//  Lime Drop
+//
+//  Created by Marshall Jones on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Lime_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
