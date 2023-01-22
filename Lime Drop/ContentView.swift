ae6fbd1f84ed4802633018e4b8f0ddf5376c5d2a
Sockzzz <86269797+Sockzzz@users.noreply.github.com>
2023-01-22 13:13:30 -0800

    Significant example on how to set up some UI elements

--- Lime Drop/ContentView.swift
+++ Lime Drop/ContentView.swift
@@ -10,13 +10,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        ZStack{ //layered elements ontop of one another
+            Image("background-cloth") //relative to name in Preview Assets
+                .resizable() //makes it stay within the bounds of 100% screen
+                .ignoresSafeArea() //allows the image to extend into the unsafe area
+            
+            VStack{ //vertical assortment of elements, equi distance apart
+                //considered a 'View Container'
+                
+                Spacer() //add or remove these to see how they work
+                //basically they push elements evenly within the bounds of space between elements in the stack
+                Image("logo")
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Image("button")
+                
+                Spacer()
+                
+                HStack{ //same as Vstack but HZ
+                    VStack{
+                        Text("CPU")
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    
+                    VStack{
+                        Text("Player")
+                            .padding(.bottom, 10.0) //padding example
+                            
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    
+                }
+                .foregroundColor(.white) //applies to the above view container and all relevant elements within the container
+                .font(.headline)
+                
+                Spacer()
+            }
+            
         }
-        .padding()
+        
+
+        
     }
 }
 
